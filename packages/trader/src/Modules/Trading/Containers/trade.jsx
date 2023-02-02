4a2dae467a85df6d6e0df9bf3dfa586feb64eb13
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-02-02 11:42:51 +0300

    refactor: glitching fix and deleted odd css

--- packages/trader/src/Modules/Trading/Containers/trade.jsx
+++ packages/trader/src/Modules/Trading/Containers/trade.jsx
@@ -37,10 +37,10 @@ const Trade = ({
     is_chart_loading,
     is_dark_theme,
     is_eu,
-    is_logging_in,
     is_market_closed,
     is_market_unavailable_visible,
     is_trade_enabled,
+    is_virtual,
     network_status,
     NotificationMessages,
     onChange,
@@ -225,10 +225,12 @@ const Trade = ({
                 <FormLayout
                     is_market_closed={is_market_closed}
                     is_trade_enabled={
-                        is_trade_enabled &&
-                        form_components.length > 0 &&
-                        network_status.class === 'online' &&
-                        !is_logging_in
+                        !is_virtual
+                            ? is_trade_enabled &&
+                              form_components.length > 0 &&
+                              network_status.class === 'online' &&
+                              !form_components.includes('accumulator')
+                            : is_trade_enabled && form_components.length > 0 && network_status.class === 'online'
                     }
                 />
             </div>
@@ -240,7 +242,7 @@ export default connect(({ client, common, modules, ui }) => ({
     getFirstOpenMarket: modules.trade.getFirstOpenMarket,
     is_accumulator: modules.trade.is_accumulator,
     is_eu: client.is_eu,
-    is_logging_in: client.is_logging_in,
+    is_virtual: client.is_virtual,
     is_synthetics_available: modules.trade.is_synthetics_available,
     is_synthetics_trading_market_available: modules.trade.is_synthetics_trading_market_available,
     network_status: common.network_status,
