4ba9b91ef4b091742f325d5549e79822aeb1b109
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-02-22 16:58:34 +0400

    chore: add feature flag for passkeys using growthbook

--- packages/hooks/src/useIsPasskeySupported.ts
+++ packages/hooks/src/useIsPasskeySupported.ts
@@ -1,16 +1,20 @@
 import React from 'react';
 import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
+import useGrowthbookFeatureFlag from './useGrowthbookFeatureFlag';
 
 const useIsPasskeySupported = () => {
     const [is_passkey_supported, setIsPasskeySupported] = React.useState(false);
     const [is_passkey_support_checking, setIsPasskeySupportChecking] = React.useState(true);
+    const is_passkeys_enabled = useGrowthbookFeatureFlag({
+        featureFlag: 'web_passkeys',
+        defaultValue: true,
+    });
 
     React.useEffect(() => {
         const checkPasskeySupport = async () => {
             try {
                 const result = await platformAuthenticatorIsAvailable();
                 //TODO: add feature flag with growthbook instead of is_passkeys_enabled here
-                const is_passkeys_enabled = true;
                 setIsPasskeySupported(result && is_passkeys_enabled);
             } catch (error) {
                 /* eslint-disable no-console */
@@ -23,7 +27,7 @@ const useIsPasskeySupported = () => {
         checkPasskeySupport();
     }, []);
 
-    return { is_passkey_supported, is_passkey_support_checking };
+    return { is_passkey_supported, is_passkey_support_checking, is_passkeys_enabled };
 };
 
 export default useIsPasskeySupported;
